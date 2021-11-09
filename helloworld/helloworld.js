2c8decdb2860a528c19b743c0c9f9eeea5f67c33
Hr <Guruprasad.Hr@tesco.com>
2021-11-09 11:29:04 +0530

    js-node

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -1,1 +1,7 @@
-console.log("Hare, श्रीनिवास​​"); 
+// indian synonym for greetings
+
+/*
+	Particularly south indian version 
+  of the typical hello world app 
+*/
+console.log("Hare, श्रीनिवास"); 
